d04f9cc217fd67eb13316b8952f94c33082a362c
Murat Seçgin <muratsecgin@Murat-MacBook-Air.local>
2023-06-14 13:02:20 +0300

    display of questions

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ Soru.prototype.cevapKontrolEt = function (cevap) {
 }
 
 let sorular = [
-    new Soru("Hangisi javascript paket yönetim uygulamasıdır?", { a: "Node.js", b: "Typescript", c: "Npm" }, "c"),
+    new Soru("Hangisi javascript paket yönetim uygulamasıdır?", { a: "Node.js", b: "Typescript", c: "Npm", d: "Nuget" }, "c"),
     new Soru("Hangisi .net paket yönetim uygulamasıdır?", { a: "Node.js", b: "Nuget", c: "Npm" }, "b"),
 ];
 
@@ -31,12 +31,66 @@ Quiz.prototype.soruGetir = function () {
 const quiz = new Quiz(sorular);
 
 document.querySelector(".btn-start").addEventListener("click", function () {
-    if (quiz.sorular.lenght != quiz.soruIndex) {
-        document.querySelector(".quiz-box").classList.add("active");
-        console.log(quiz.soruGetir());
+    document.querySelector(".quiz-box").classList.add("active");
+    soruGoster(quiz.soruGetir());
+    document.querySelector(".next-btn").classList.remove("show");
+})
+
+document.querySelector(".next-btn").addEventListener("click", function () {
+    if (quiz.sorular.lenght != quiz.soruIndex + 1) {
         quiz.soruIndex += 1;
+        soruGoster(quiz.soruGetir());
+        document.querySelector(".next-btn").classList.remove("show");
     } else {
         console.log("Quiz bitti!")
     }
 })
+
+const option_list = document.querySelector(".option-list");
+const correctIcon = '<div class="icon"><i class="fas fa-check"></i></div>';
+const incorrectIcon = '<div class="icon"><i class="fas fa-times"></i></div>'
+
+function soruGoster(soru) {
+    let question = `<span>${soru.soruMetni}</span>`;
+    let options = "";
+
+    for (let cevap in soru.cevapSecenekleri) {
+        options +=
+            `
+            <div class="option">
+                <span><b>${cevap}</b>: ${soru.cevapSecenekleri[cevap]}</span>
+            </div>
+        `;
+    }
+
+    document.querySelector(".question-text").innerHTML = question;
+    option_list.innerHTML = options;
+
+    const option = option_list.querySelectorAll(".option");
+
+    for (let opt of option) {
+        opt.setAttribute("onclick", "optionSelected(this)")
+    }
+}
+
+function optionSelected(option) {
+    let cevap = option.querySelector("span b").textContent;
+    let soru = quiz.soruGetir();
+
+    if (soru.cevapKontrolEt(cevap)) {
+        option.classList.add("correct");
+        option.insertAdjacentHTML("beforeend", correctIcon);
+    } else {
+        option.classList.add("incorrect");
+        option.insertAdjacentHTML("beforeend", incorrectIcon);
+
+    }
+
+    for (let i = 0; i < option_list.children.length; i++) {
+        option_list.children[i].classList.add("disabled");
+    }
+
+    document.querySelector(".next-btn").classList.add("show");
+
+}
 
