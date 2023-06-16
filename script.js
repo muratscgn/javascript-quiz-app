7447f798f9da46ce973ed20d6279a1b5027c115d
Murat Seçgin <muratsecgin@192.168.1.114>
2023-06-16 12:25:46 +0300

    response time

--- script.js
+++ script.js
@@ -3,27 +3,46 @@ const ui = new UI();
 
 ui.btn_start.addEventListener("click", function () {
     ui.quiz_box.classList.add("active");
+    startTimer(10);
     ui.soruGoster(quiz.soruGetir());
     ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.lenght);
     ui.btn_next.classList.remove("show");
 })
 
 ui.btn_next.addEventListener("click", function () {
-    if (quiz.sorular.lenght != quiz.soruIndex + 1) {
+    if (quiz.sorular.length != quiz.soruIndex + 1) {
         quiz.soruIndex += 1;
+        clearInterval(counter);
+        startTimer(10);
         ui.soruGoster(quiz.soruGetir());
-        ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.lenght);
+        ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
         ui.btn_next.classList.remove("show");
     } else {
-        console.log("Quiz bitti!")
+        clearInterval(counter);
+        ui.quiz_box.classList.remove("active");
+        ui.score_box.classList.add("active");
+        ui.skoruGoster(quiz.sorular.lenght, quiz.dogruCevapSayisi);
     }
-})
+});
+
+ui.btn_quit.addEventListener("click", function () {
+    window.location.reload();
+});
+
+ui.btn_replay.addEventListener("click", function () {
+    quiz.soruIndex = 0;
+    quiz.dogruCevapSayisi = 0;
+    ui.btn_start.click();
+    ui.score_box.classList.remove("active");
+});
 
 function optionSelected(option) {
+    clearInterval(counter);
     let cevap = option.querySelector("span b").textContent;
     let soru = quiz.soruGetir();
 
     if (soru.cevapKontrolEt(cevap)) {
+        quiz.dogruCevapSayisi += 1;
         option.classList.add("correct");
         option.insertAdjacentHTML("beforeend", ui.correctIcon);
     } else {
@@ -38,4 +57,32 @@ function optionSelected(option) {
 
     ui.btn_next.classList.add("show");
 
+}
+
+let counter;
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+
+    function timer() {
+        ui.time_second.textContent = time;
+        time--;
+
+        if (time < 0) {
+            clearInterval(counter);
+
+            ui.time_text.textContent = "Süre bitti!";
+
+            let cevap = quiz.soruGetir().dogruCevap;
+
+            for (let option of ui.option_list.children) {
+
+                if (option.querySelector("span b").textContent == cevap) {
+                    option.classList.add("correct");
+                    option.insertAdjacentHTML("beforeend", ui.correctIcon);
+                }
+                option.classList.add("disabled");
+            }
+            ui.btn_next.classList.add("show");
+        }
+    }
 }
--- ui.js
+++ ui.js
@@ -1,10 +1,15 @@
 function UI() {
     this.btn_start = document.querySelector(".btn-start"),
         this.btn_next = document.querySelector(".next-btn"),
+        this.btn_replay = document.querySelector(".btn_replay"),
+        this.btn_quit = document.querySelector(".btn_quit"),
         this.quiz_box = document.querySelector(".quiz-box"),
+        this.score_box = document.querySelector(".score_box"),
         this.option_list = document.querySelector(".option-list"),
         this.correctIcon = '<div class="icon"><i class="fas fa-check"></i></div>',
-        this.incorrectIcon = '<div class="icon"><i class="fas fa-times"></i></div>'
+        this.incorrectIcon = '<div class="icon"><i class="fas fa-times"></i></div>',
+        this.time_text = document.querySelector(".time_text"),
+        this.time_second = document.querySelector(".time_second")
 }
 
 UI.prototype.soruGoster = function (soru) {
@@ -33,4 +38,9 @@ UI.prototype.soruGoster = function (soru) {
 UI.prototype.soruSayisiniGoster = function (soruSirasi) {
     let tag = `<span class="badge bg-warning"> ${soruSirasi} / ${sorular.length} </span>`;
     document.querySelector(".quiz-box .question-index").innerHTML = tag;
+}
+
+UI.prototype.skoruGoster = function (toplamSoru, dogruCevap) {
+    let tag = `Toplam ${sorular.length} sorudan ${dogruCevap} doğru cevap.`;
+    document.querySelector(".score_box .score_text").innerHTML = tag;
 }
